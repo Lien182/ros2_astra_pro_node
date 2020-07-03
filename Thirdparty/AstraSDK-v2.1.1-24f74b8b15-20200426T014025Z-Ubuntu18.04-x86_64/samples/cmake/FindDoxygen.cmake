7a78112cdf3878dc8afb2ff02e6d30f39aa3517a
Christian Lienen <christianlienen@web.de>
2020-07-03 08:54:04 +0200

    no error at compile time, but segfault

--- Thirdparty/AstraSDK-v2.1.1-24f74b8b15-20200426T014025Z-Ubuntu18.04-x86_64/samples/cmake/FindDoxygen.cmake
+++
@@ -1,7 +0,0 @@
-find_program(DOXYGEN_EXECUTABLE
-  doxygen
-  PATHS /usr/local/bin)
-
-include(FindPackageHandleStandardArgs)
-
-find_package_handle_standard_args(doxygen DEFAULT_MSG DOXYGEN_EXECUTABLE)
