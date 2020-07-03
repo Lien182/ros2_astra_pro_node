70672d64c94b4cbfeb6da131c7b2eb01cb0bb019
Christian Lienen <christianlienen@web.de>
2020-07-03 09:29:46 +0200

    main merged to one listener for depth and color

--- src/main.cpp
+++ src/main.cpp
@@ -14,28 +14,86 @@
 // limitations under the License.
 //
 // Be excellent to each other.
+
+
 #include <astra/astra.hpp>
 #include <cstdio>
+#include <chrono>
 #include <iostream>
+#include <iomanip>
 #include <key_handler.h>
 
-class SampleFrameListener : public astra::FrameListener
+
+class FrameListener : public astra::FrameListener
 {
 private:
-    using buffer_ptr = std::unique_ptr<astra::RgbPixel []>;
-    buffer_ptr buffer_;
-    unsigned int lastWidth_;
-    unsigned int lastHeight_;
+    using depthbuffer_ptr = std::unique_ptr<int16_t []>;
+    depthbuffer_ptr depthbuffer_;
+    unsigned int depthlastWidth_;
+    unsigned int depthlastHeight_;
+
+    using colorbuffer_ptr = std::unique_ptr<astra::RgbPixel []>;
+    colorbuffer_ptr colorbuffer_;
+    unsigned int colorlastWidth_;
+    unsigned int colorlastHeight_;
 
 public:
     virtual void on_frame_ready(astra::StreamReader& reader,
                                 astra::Frame& frame) override
     {
+        const astra::DepthFrame depthFrame = frame.get<astra::DepthFrame>();
+
+        if (depthFrame.is_valid())
+        {
+            auto depthStream = reader.stream<astra::DepthStream>();
+            print_depth(depthFrame, depthStream.coordinateMapper());
+            check_fps();
+        }
+
         const astra::ColorFrame colorFrame = frame.get<astra::ColorFrame>();
 
         if (colorFrame.is_valid())
         {
             print_color(colorFrame);
+        }
+
+    }
+
+    void print_depth(const astra::DepthFrame& depthFrame,
+                     const astra::CoordinateMapper& mapper)
+    {
+        if (depthFrame.is_valid())
+        {
+            int width = depthFrame.width();
+            int height = depthFrame.height();
+            int frameIndex = depthFrame.frame_index();
+
+            //determine if buffer needs to be reallocated
+            if (width != depthlastWidth_ || height != depthlastHeight_)
+            {
+                depthbuffer_ = depthbuffer_ptr(new int16_t[depthFrame.length()]);
+                depthlastWidth_ = width;
+                depthlastHeight_ = height;
+            }
+            depthFrame.copy_to(depthbuffer_.get());
+
+            size_t index = ((width * (height / 2.0f)) + (width / 2.0f));
+            short middle = depthbuffer_[index];
+
+            float worldX, worldY, worldZ;
+            float depthX, depthY, depthZ;
+            mapper.convert_depth_to_world(width / 2.0f, height / 2.0f, middle, worldX, worldY, worldZ);
+            mapper.convert_world_to_depth(worldX, worldY, worldZ, depthX, depthY, depthZ);
+
+            std::cout << "depth frameIndex: " << frameIndex
+                      << " value: " << middle
+                      << " wX: " << worldX
+                      << " wY: " << worldY
+                      << " wZ: " << worldZ
+                      << " dX: " << depthX
+                      << " dY: " << depthY
+                      << " dZ: " << depthZ
+                      << std::endl;
         }
     }
 
@@ -47,15 +105,15 @@ public:
             int height = colorFrame.height();
             int frameIndex = colorFrame.frame_index();
 
-            if (width != lastWidth_ || height != lastHeight_){
-                buffer_ = buffer_ptr(new astra::RgbPixel[colorFrame.length()]);
-                lastWidth_ = width;
-                lastHeight_ = height;
+            if (width != colorlastWidth_ || height != colorlastHeight_){
+                colorbuffer_ = colorbuffer_ptr(new astra::RgbPixel[colorFrame.length()]);
+                colorlastWidth_ = width;
+                colorlastHeight_ = height;
             }
-            colorFrame.copy_to(buffer_.get());
+            colorFrame.copy_to(colorbuffer_.get());
 
             size_t index = ((width * (height / 2.0f)) + (width / 2.0f));
-            astra::RgbPixel middle = buffer_[index];
+            astra::RgbPixel middle = colorbuffer_[index];
 
             std::cout << "color frameIndex: " << frameIndex
                       << " r: " << static_cast<int>(middle.r)
@@ -64,6 +122,35 @@ public:
                       << std::endl;
         }
     }
+
+    void check_fps()
+    {
+        const float frameWeight = 0.5;
+
+        auto newTimepoint = clock_type::now();
+        float frameDuration = std::chrono::duration_cast<duration_type>(newTimepoint - lastTimepoint_).count();
+
+        frameDuration_ = frameDuration * frameWeight + frameDuration_ * (1 - frameWeight);
+        lastTimepoint_ = newTimepoint;
+
+        double fps = 1.0 / frameDuration_;
+
+        auto precision = std::cout.precision();
+        std::cout << std::fixed
+                  << std::setprecision(1)
+                  << fps << " fps ("
+                  << std::setprecision(2)
+                  << frameDuration_ * 1000.0 << " ms)"
+                  << std::setprecision(precision)
+                  << std::endl;
+    }
+
+private:
+    using duration_type = std::chrono::duration<float>;
+    float frameDuration_{ 0.0 };
+
+    using clock_type = std::chrono::system_clock;
+    std::chrono::time_point<clock_type> lastTimepoint_{clock_type::now()};
 };
 
 int main(int argc, char** argv)
@@ -75,7 +162,35 @@ int main(int argc, char** argv)
     astra::StreamSet streamSet;
     astra::StreamReader reader = streamSet.create_reader();
 
-    SampleFrameListener listener;
+    //New
+    FrameListener listener;
+    reader.stream<astra::ColorStream>().start();
+    //Endnew
+
+    //DepthFrameListener depthListener;
+
+    auto depthStream = reader.stream<astra::DepthStream>();
+    depthStream.start();
+
+
+    reader.add_listener(listener);
+
+    do{
+        astra_update();
+    } while (shouldContinue);
+
+    reader.remove_listener(listener);
+    astra::terminate();
+
+
+//old main
+
+/*
+
+    astra::StreamSet streamSet;
+    astra::StreamReader reader = streamSet.create_reader();
+
+    ColorFrameListener colorlistener;
 
     reader.stream<astra::ColorStream>().start();
 
@@ -95,4 +210,5 @@ int main(int argc, char** argv)
     reader.remove_listener(listener);
 
     astra::terminate();
+ */   
 }
